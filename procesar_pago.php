34391723e6dee05910682c4b46723b189ba2d4ea
Guty <alejandrocesar935@gmail.com>
2025-12-10 08:12:24 +0000

    CAMBIOS BRUTALES

--- procesar_pago.php
+++ procesar_pago.php
@@ -1,5 +1,6 @@
 <?php
-require_once __DIR__ . '/../database/config.php';
+require_once __DIR__ . "/database/config.php";
+
 verificarSesion();
 
 if ($_SERVER['REQUEST_METHOD'] !== 'POST') {
--- sesion.php
+++ sesion.php
@@ -3,7 +3,8 @@
 ini_set('display_errors', 1);
 error_reporting(E_ALL);
 
-require_once __DIR__ . '/../database/config.php';
+require_once __DIR__ . "/database/config.php";
+
 
 $error = '';
 $success = '';
