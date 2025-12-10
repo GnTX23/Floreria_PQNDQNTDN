5a1dbde1633506c6115d505ad5c0724f05efb332
Guty <alejandrocesar935@gmail.com>
2025-12-10 05:37:11 +0000

    Cambio config.php v2

--- database/config.php
+++ database/config.php
@@ -1,17 +1,20 @@
-<?php
-// Configuración para Railway (producción)
+<<?php
+
+
 define('DB_HOST', 'caboose.proxy.rlwy.net'); 
-define('DB_PORT', '15501');                    
+define('DB_PORT', 15501);                    
 define('DB_USER', 'root');                     
 define('DB_PASS', 'zrujEdYSgTqSeUDjWWFeAYbVfifgPJvT');  
 define('DB_NAME', 'railway');                  
+
 function conectarDB() {
-    $conn = new mysqli(DB_HOST, DB_PORT, DB_USER, DB_PASS, DB_NAME); 
     
+    $conn = new mysqli(DB_HOST, DB_USER, DB_PASS, DB_NAME, DB_PORT);
+
     if ($conn->connect_error) {
         die("Error de conexión: " . $conn->connect_error);
     }
-    
+
     $conn->set_charset("utf8mb4");
     return $conn;
 }
